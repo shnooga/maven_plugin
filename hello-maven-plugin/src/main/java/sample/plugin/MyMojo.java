616a1d508e786f85415764ca8acf2d5d5ee73f36
Jane Saint <papertrail@gmail.com>
2014-11-06 23:02:53 -0800

    Cleaning up code

--- hello-maven-plugin/src/main/java/sample/plugin/MyMojo.java
+++ hello-maven-plugin/src/main/java/sample/plugin/MyMojo.java
@@ -1,48 +1,26 @@
 package sample.plugin;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 import java.io.*;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.logging.*;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.*;
 
-/**
- * Goal which touches a timestamp file.
- *
- * @deprecated Don't use!
- */
 @Mojo(name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class MyMojo extends AbstractMojo {
 	private String[] flaggedSyntax = {"import .*", "ruleflow-group .*"};
 
 	@Parameter(defaultValue = "${basedir}/myrule.drl", property = "inputFile", required = true)
 	private String inputFile;
+
 	@Parameter(defaultValue = "${basedir}/src/main/resources", property = "outputDir", required = true)
 	private File outputDirectory;
 
 	public void execute() throws MojoExecutionException {
 		File f = outputDirectory;
 
-		if (!f.exists()) {
+		if (!f.exists()) 
 			f.mkdirs();
-		}
 
 		File touch = new File(f, "touch.txt");
 
@@ -66,9 +44,7 @@ public class MyMojo extends AbstractMojo {
 		System.out.println(inputTxt);
 	}
 
-
 	private boolean containsFlaggedSyntax(String text) {
-//		return getFlaggedSyntax().contains(text);
 		for (String syntax: flaggedSyntax) 
 			if(text.matches(syntax))
 				return true;
@@ -82,10 +58,11 @@ public class MyMojo extends AbstractMojo {
 			FileReader fr = new FileReader(new File(fileName));
 			BufferedReader br = new BufferedReader(fr);
 			String line;
-			while ((line = br.readLine()) != null) {
+
+			while ((line = br.readLine()) != null) 
 				if(!containsFlaggedSyntax(line))
 					sb.append(line).append("\n");
-			}
+
 			br.close();
 			fr.close();
 		} catch (FileNotFoundException ex) {
