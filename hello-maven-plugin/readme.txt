d86d562749c60bc6928af64f932047ed13a0a858
Jane Saint <papertrail@gmail.com>
2014-11-19 00:48:58 -0800

    Testing of clean goal is successful

--- hello-maven-plugin/readme.txt
+++ hello-maven-plugin/readme.txt
@@ -1,13 +1,18 @@
-# To fire off the plugin
+# To create new rule test
 mvn com.vsp.enterprise.test:ruletest:create 
-# Required
 -DinputFile=./myrule.drl 
 # Optional
 -DoverwriteExistJavaTest=true 
--DjavaTestDir=. 
--DresourceDir=. 
+-DjavaTestDir=./src/test/java
+-DresourceDir=./src/main/resources 
 -DtemplateFile=./src/main/resources/ruletesttemplate.txt
 
 
 # To clean off the files
-mvn com.vsp.enterprise.test:ruletest:clean
+mvn com.vsp.enterprise.test:ruletest:clean 
+-DinputFile=./myrule.drl 
+#Optional
+-DresourceDir=./src/main/java/resources 
+
+// Creating Maven plugin
+mvn archetype:generate -DgroupId=sample.plugin -DartifactId=hello-maven-plugin -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-plugin
--- hello-maven-plugin/src/main/java/com/vsp/enterprise/test/RuleCleanUpMojo.java
+++ hello-maven-plugin/src/main/java/com/vsp/enterprise/test/RuleCleanUpMojo.java
@@ -1,7 +1,6 @@
 package com.vsp.enterprise.test;
 
 import java.io.*;
-import java.util.logging.*;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.*;
@@ -9,58 +8,23 @@ import org.apache.maven.plugins.annotations.*;
 @Mojo(name = "clean", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class RuleCleanUpMojo extends AbstractMojo {
 	public static final String RULE_NAME_MARKER = "Faux";
-	public static final String JAVA_NAME_MARKER = "Test";
-	private static final String[] flaggedDroolSyntax = {"import .*", ".*ruleflow-group .*"};
 
-	@Parameter(defaultValue = "${basedir}/myrule.drl", property = "inputFile", required = true)
+	@Parameter(property = "inputFile", required = true)
 	private String inputFile;
 
-	@Parameter(defaultValue = "${basedir}/src/main/resources", property = "resourceDir", required = true)
+	@Parameter(defaultValue = "${basedir}/src/main/resources", property = "resourceDir")
 	private String resourceDirectory;
-
-	@Parameter(defaultValue = "${basedir}/src/test/java/com/vsp/enterprise/test", property = "javaTestDir", required = true)
-	private String javaTestDirectory;
-
-	@Parameter(defaultValue = "${basedir}/src/main/resources/ruletesttemplate.txt", property = "templateFile", required = true)
-	private String templateRuleFile;
 
 	public void execute() throws MojoExecutionException {
 		FileNameManipulator nameManipulator = new FileNameManipulator(inputFile);
 		
 		nameManipulator = new FileNameManipulator(resourceDirectory + File.separator + nameManipulator.extractFileName());
 		String fullyQualifiedFileName = nameManipulator.postPendTextToFileName(RULE_NAME_MARKER);
-		String ruleFileName = FileNameManipulator.extractFileName(fullyQualifiedFileName);
+		File fauxDroolFile = new File(fullyQualifiedFileName);
 
-		fileExists(fullyQualifiedFileName, readDroolFile(inputFile));
-
-		String[] fileNamePaths = nameManipulator.splitFileName();
-
-		nameManipulator = new FileNameManipulator(javaTestDirectory + File.separator + fileNamePaths[1] + ".java");
-		fullyQualifiedFileName = nameManipulator.postPendTextToFileName(JAVA_NAME_MARKER);
-		String javaFileName = FileNameManipulator.splitFileName(fullyQualifiedFileName)[1];
-	}
-
-	private void fileExists(String fileName, String text) throws MojoExecutionException {
-		FileWriter w = null;
-		try {
-			w = new FileWriter(new File(fileName));
-			w.write(text);
-		} catch (IOException e) {
-			throw new MojoExecutionException("Error creating file " + fileName, e);
-		} finally {
-			if (w != null) {
-				try {
-					w.close();
-				} catch (IOException e) {
-					// ignore
-				}
-			}
+		if (fauxDroolFile.exists()) {
+			System.out.println("Deleting " + fullyQualifiedFileName);
+			fauxDroolFile.delete();
 		}
 	}
-
-	private String readDroolFile(String fileName) {
-		StringBuilder sb = new StringBuilder();
-		return sb.toString();
-	}
-
 }
